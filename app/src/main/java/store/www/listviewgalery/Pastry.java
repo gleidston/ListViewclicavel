a26f978225d70fb6529cf6305eb87fe9f2c59386
Gleidston <king2010-ro@hotmail.com>
2019-10-03 19:41:38 -0300

    exercicio 5 Desenvolva uma lista com dois itens e na segunda tela uma galeria de fotos do item clicado
     criado uma lista com 4 itens ndepois a foto do inten mais incremento e
     decremento como se fosse carrio de compra ecomerce
    
     baseado no artigo
    
     REFERENCE: https://developer.android.com/guide/topics/ui/layout/listview
     https://developer.android.com/reference/android/widget/ListView

---
+++ app/src/main/java/store/www/listviewgalery/Pastry.java
@@ -0,0 +1,14 @@
+package store.www.listviewgalery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Pastry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pastry);
+    }
+}
